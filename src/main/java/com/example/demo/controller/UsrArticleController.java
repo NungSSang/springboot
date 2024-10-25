e65a4c08812ce4d3c85f8037ec65d7dda456bdf3
NungSSang <kwj980421@naver.com>
2024-10-25 16:12:00 +0900

    새로 추가한 것들

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -8,47 +7,126 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.dto.Article;
+import com.example.demo.dto.ResultData;
+import com.example.demo.service.ArticleService;
+import com.example.demo.util.Util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UsrArticleController {
-
-	Article article;
-	int lastArticleId;
-	List<Article> articles = new ArrayList<>();
-	UsrArticleService uas;
-
-	public UsrArticleController() {
-		this.lastArticleId = 0;
-		this.articles = new ArrayList<>();
+	
+	private ArticleService articleService;
+	
+	public UsrArticleController(ArticleService articleService) {
+		this.articleService = articleService;
 	}
+	
 
 	@GetMapping("/usr/article/doWrite")
 	@ResponseBody
-	public Article doWrite(String title, String body) {
-		lastArticleId++;
-		Article article = new Article(lastArticleId, title, body);
-		articles.add(article);
-		return article;
+	public ResultData<Article> doWrite(HttpSession session, String title, String body) {
+		
+		int loginedMemberId = -1;
+		
+		if (session.getAttribute("loginedMemberId") != null) {
+			loginedMemberId = (int) session.getAttribute("loginedMemberId");
+		}
+		
+		if (loginedMemberId == -1) {
+			return ResultData.from("F-1", "로그인 후 이용할 수 있는 기능입니다");
+		}
+		
+		if (Util.isEmpty(title)) {
+			return ResultData.from("F-2", "제목을 입력해주세요");
+		}
+		
+		if (Util.isEmpty(body)) {
+			return ResultData.from("F-3", "내용을 입력해주세요");
+		}
+		
+		articleService.writeArticle(loginedMemberId, title, body);
+		
+		int id = articleService.getLastInsertId();
+		
+		return ResultData.from("S-1", String.format("%d번 게시물을 작성했습니다", id), articleService.getArticleById(id));
 	}
 
 	@GetMapping("/usr/article/showList")
 	@ResponseBody
-	public List<Article> showList() {
-		return articles;
+	public ResultData<List<Article>> showList(HttpServletRequest requst) {
+		List<Article> articles = articleService.getArticles();
+		
+		if (articles.size() == 0) {
+			return ResultData.from("F-1", "게시물이 존재하지 않습니다");
+		}
+		
+		return ResultData.from("S-1", "리스트 조회 성공", articles);
 	}
 	
-
-//	@GetMapping("/usr/article/detail")
-//	@ResponseBody
-//	public List<Article> detail(int id) {
-//		for(int i = 0; i < articles.size(); i++) {
-//			if(article.getId() == id) {
-//				return articles;
-//			}else {
-//				Article article = new Article(0,"없음","없음");
-//				return  article;
-//			}
-//		}
-//	}
 	
+	@GetMapping("/usr/article/showDetail")
+	@ResponseBody
+	public ResultData<Article> showDetail(int id) {
+
+		Article foundArticle = articleService.getArticleById(id);
+		
+		
+		if (foundArticle == null) {
+			return ResultData.from("F-1", String.format("%d번 게시물은 존재하지 않습니다", id));
+		}
+
+		
+		return ResultData.from("S-1", String.format("%d번 게시물 상세보기", id), foundArticle);
+	}
+	
+	@GetMapping("/usr/article/doModify")
+	@ResponseBody
+	public ResultData<Article> doModify(HttpSession session, int id, String title, String body) {
+		
+		Article foundArticle = articleService.getArticleById(id);
+		int loginedMemberId = -1;
+		
+		if (session.getAttribute("loginedMemberId") != null) {
+			loginedMemberId = (int) session.getAttribute("loginedMemberId");
+		}
+		
+		if (loginedMemberId == -1) {
+			return ResultData.from("F-1", "로그인 후 이용할 수 있는 기능입니다");
+		}		
+		if (foundArticle == null) {
+			return ResultData.from("F-1", String.format("%d번 게시물은 존재하지 않습니다", id));
+		}
+		if(loginedMemberId != foundArticle.getMemberId()) {
+			return ResultData.from("F-1", String.format("%d번 게시물은 본인이 작성하지 않았습니다.", id));
+		}
+			articleService.modifyArticle(id, title, body);
+			return ResultData.from("S-1", String.format("%d번 게시물을 수정했습니다", id), articleService.getArticleById(id));
+	}
+
+	@GetMapping("/usr/article/doDelete")
+	@ResponseBody
+	public ResultData doDelete(HttpSession session, int id) {
+		
+		Article foundArticle = articleService.getArticleById(id);
+		int loginedMemberId = -1;
+		
+		if (session.getAttribute("loginedMemberId") != null) {
+			loginedMemberId = (int) session.getAttribute("loginedMemberId");
+		}
+		
+		if (loginedMemberId == -1) {
+			return ResultData.from("F-1", "로그인 후 이용할 수 있는 기능입니다");
+		}
+		if (foundArticle == null) {
+			return ResultData.from("F-1", String.format("%d번 게시물은 존재하지 않습니다", id));
+		}
+		if(loginedMemberId != foundArticle.getMemberId()) {
+			return ResultData.from("F-1", String.format("%d번 게시물은 본인이 작성하지 않았습니다.", id));
+		}
+			articleService.deleteArticle(id);
+			return ResultData.from("S-1", String.format("%d번 게시물을 삭제했습니다", id));
+		
+	}
 }
--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -6,19 +6,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-	int i = -1;
 	
-	
-	@GetMapping("/user/home/main") // 이게 요청
-	@ResponseBody //아래의 함수 리턴을 통해 응답
-	public String showMain() {
-		return "Hello 안녕하세요";
-	}
-	
-	@GetMapping("/user/home/int")
+	@GetMapping("/usr/home/main")
 	@ResponseBody
-	public int test1() {
-		return ++i;
+	public String showMain() {
+		return "안녕하세요";
 	}
-	
 }
